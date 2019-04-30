cd08094e130a2183e468f2d59d32fdd0d72e45e2
君島克弥 <K.Kimishima123@gmail.com>
2019-04-30 18:17:44 +0900

    first commit

---
+++ src/main/java/com/github/kkimishima/gradlecreatetest/GradleCreateTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.kkimishima.gradlecreatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCreateTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GradleCreateTestApplication.class, args);
+  }
+
+}
